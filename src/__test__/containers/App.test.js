2d716b18b0bacaea64fe4b6d873b9d68b4c69ec3
박민호 <radlohead@naver.com>
2019-02-21 23:51:25 +0900

    App 테스트코드 작성

---
+++ src/__test__/containers/App.test.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../../containers/App';
+import store from '../../store';
+import { Home, Menu, Join } from '../../components';
+configure({ adapter: new Adapter() });
+
+it('App test', () => {
+    const component = mount(
+        <Provider store={store}>
+            <BrowserRouter>
+                <Route>
+                    <App />
+                </Route>
+            </BrowserRouter>
+        </Provider>
+    );
+
+    const pathMap = component.find(Route).reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+    }, {});
+    
+    expect(component.find(Menu).length).toEqual(1);
+    expect(pathMap['/']).toBe(Home);
+    expect(pathMap['/join']).toBe(Join);
+});
