b3909fcec7e91e5fbfd6b54021521791457156d5
박민호 <radlohead@naver.com>
2019-03-03 15:39:23 +0900

    LoginForm 테스트 수정

--- src/__test__/components/LoginForm.test.js
+++ src/__test__/components/LoginForm.test.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { shallow, configure } from 'enzyme';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import LoginForm from '../../components/LoginForm';
+import store from '../../store';
 configure({ adapter: new Adapter() });
 
 it('LoginForm test', () => {
-    const component = shallow(<LoginForm />);
-    expect(component).toBeTruthy();
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <LoginForm />
+        </Provider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
 });
